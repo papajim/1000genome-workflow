5e1e36bcfdf4d537e09043ff0f11170cf5ee3d35
George Papadimitriou <georgpap@isi.edu>
2018-12-01 12:45:10 -0800

    Added data and removed Adamant specific things

--- daxgen.py
+++ daxgen.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import csv
+from datetime import datetime
 from Pegasus.DAX3 import *
 from optparse import OptionParser
 
@@ -13,47 +14,46 @@ parser = OptionParser(usage="usage: %prog [options]", version="%prog 1.0")
 parser.add_option('-d', '--dax', action='store', dest='daxfile', default='1000genome.dax', help='DAX filename')
 parser.add_option('-D', '--dataset', action='store', dest='dataset', default='20130502', help='Dataset folder')
 parser.add_option('-f', '--datafile', action='store', dest='datafile', default='data.csv', help='Data file with list of input data')
-parser.add_option('-y', '--priority', action='store', dest='priority', default='70', help='Workflow Priority')
-parser.add_option('-s', '--site', action='store', dest='sitename', default='S1', help='Site name')
-parser.add_option('-u', '--user', action='store', dest='user', default='panorama', help='RabbitMQ username')
-parser.add_option('-p', '--passwd', action='store', dest='passwd', default='panorama', help='RabbitMQ password')
-parser.add_option('-H', '--host', action='store', dest='host', default='128.9.136.208', help='RabbitMQ hostname or IP')
-parser.add_option('-P', '--port', action='store', dest='port', default='5671', help='RabbitMQ port')
-parser.add_option('-n', '--no-ssl', action='store_false', dest='ssl', default=True, help='RabbitMQ SSL connection')
 
 (options, args) = parser.parse_args()
 
 base_dir = os.path.abspath('.')
 
 # Create a abstract dag
-workflow = ADAG("1000genome-%s" % options.dataset)
+ts = datetime.utcnow().strftime("%Y%m%dT%H%M%SZ")
+#workflow = ADAG("1000genome-%s" % options.dataset)
+workflow = ADAG("1000genome-%s" % ts)
 
 # Executables
 e_individuals = Executable('individuals', arch='x86_64', installed=False)
-e_individuals.addPFN(PFN('scp://adamant@master' + base_dir + '/bin/individuals', 'local'))
+e_individuals.addPFN(PFN('file://' + base_dir + '/bin/individuals', 'local'))
 workflow.addExecutable(e_individuals)
 
 e_individuals_merge = Executable('individuals_merge', arch='x86_64', installed=False)
-e_individuals_merge.addPFN(PFN('scp://adamant@master' + base_dir + '/bin/individuals_merge', 'local'))
+e_individuals_merge.addPFN(PFN('file://' + base_dir + '/bin/individuals_merge', 'local'))
 workflow.addExecutable(e_individuals_merge)
 
 e_sifting = Executable('sifting', arch='x86_64', installed=False)
-e_sifting.addPFN(PFN('scp://adamant@master' + base_dir + '/bin/sifting', 'local'))
+e_sifting.addPFN(PFN('file://' + base_dir + '/bin/sifting', 'local'))
 workflow.addExecutable(e_sifting)
 
 e_mutation = Executable('mutation_overlap', arch='x86_64', installed=False)
-e_mutation.addPFN(PFN('scp://adamant@master' + base_dir + '/bin/mutation_overlap.py', 'local'))
+e_mutation.addPFN(PFN('file://' + base_dir + '/bin/mutation_overlap.py', 'local'))
 workflow.addExecutable(e_mutation)
 
 e_freq = Executable('frequency', arch='x86_64', installed=False)
-e_freq.addPFN(PFN('scp://adamant@master' + base_dir + '/bin/frequency.py', 'local'))
+e_freq.addPFN(PFN('file://' + base_dir + '/bin/frequency.py', 'local'))
 workflow.addExecutable(e_freq)
+
+f_columns = File('columns.txt')
+f_columns.addPFN(PFN('file://' + base_dir + '/data/' + options.dataset + '/columns.txt', 'condorpool'))
+workflow.addFile(f_columns)
 
 # Population Files
 populations = []
 for base_file in os.listdir('data/populations'):
   f_pop = File(base_file)
-  f_pop.addPFN(PFN('scp://adamant@master' + os.path.abspath('data/populations') + '/' + base_file, 'local'))
+  f_pop.addPFN(PFN('file://' + base_dir + '/data/populations' + '/' + base_file, 'local'))
   workflow.addFile(f_pop)
   populations.append(f_pop)
 
@@ -75,7 +75,7 @@ for row in datacsv:
 
   # Individuals Jobs
   f_individuals = File(base_file)
-  f_individuals.addPFN(PFN('http://172.16.1.200/adamant/genome/data/' + options.dataset + '/' + base_file, 'compute'))
+  f_individuals.addPFN(PFN('file://' + base_dir + '/data/' + options.dataset + '/' + base_file, 'condorpool'))
   workflow.addFile(f_individuals)
 
   c_num = base_file[base_file.find('chr')+3:]
@@ -89,17 +89,11 @@ for row in datacsv:
     output_files.append(out_name)
     f_chrn = File(out_name)
 
-    f_columns = File('columns.txt')
-
     j_individuals = Job(name='individuals')
     j_individuals.uses(f_individuals, link=Link.INPUT)
-    j_individuals.uses(f_chrn, link=Link.OUTPUT, transfer=False)
-    j_individuals.uses(f_columns, link=Link.OUTPUT, transfer=False)
+    j_individuals.uses(f_columns, link=Link.INPUT)
+    j_individuals.uses(f_chrn, link=Link.OUTPUT, transfer=False, register=False)
     j_individuals.addArguments(f_individuals, c_num, str(counter), str(stop), str(threshold))
-
-    # Pre-script (RabbitMQ)
-    j_individuals.addProfile(Profile(Namespace.DAGMAN, 'PRE', os.getcwd() + '/bin/rabbitmq'))
-    j_individuals.addProfile(Profile(Namespace.DAGMAN, 'PRE.ARGUMENTS', '%s %s %s %s %s %s %s' % (options.user, options.passwd, options.priority, options.sitename, options.host, options.port, options.ssl)))
 
     individuals_jobs.append(j_individuals)
     workflow.addJob(j_individuals)
@@ -118,7 +112,7 @@ for row in datacsv:
   individuals_filename = 'chr%sn.tar.gz' % c_num
   f_chrn_merged = File(individuals_filename)
   individuals_files.append(f_chrn_merged)
-  j_individuals_merge.uses(f_chrn_merged, link=Link.OUTPUT, transfer=False)
+  j_individuals_merge.uses(f_chrn_merged, link=Link.OUTPUT, transfer=False, register=False)
 
   workflow.addJob(j_individuals_merge)
   individuals_merge_jobs.append(j_individuals_merge)
@@ -130,7 +124,7 @@ for row in datacsv:
   j_sifting = Job(name='sifting')
   
   f_sifting = File(row[2])
-  f_sifting.addPFN(PFN('http://172.16.1.200/adamant/genome/data/' + options.dataset + '/sifting/' + row[2], 'compute'))
+  f_sifting.addPFN(PFN('file://' + base_dir +'/data/' + options.dataset + '/sifting/' + row[2], 'condorpool'))
   workflow.addFile(f_sifting)
 
   f_sifted = File('sifted.SIFT.chr%s.txt' % c_num)
@@ -138,7 +132,7 @@ for row in datacsv:
 
   j_sifting = Job(name='sifting')
   j_sifting.uses(f_sifting, link=Link.INPUT)
-  j_sifting.uses(f_sifted, link=Link.OUTPUT, transfer=False)
+  j_sifting.uses(f_sifted, link=Link.OUTPUT, transfer=False, register=False)
   j_sifting.addArguments(f_sifting, c_num)
 
   workflow.addJob(j_sifting)
@@ -156,7 +150,7 @@ for i in range(len(individuals_files)):
     j_mutation.uses(f_columns, link=Link.INPUT)
 
     f_mut_out = File('chr%s-%s.tar.gz' % (c_nums[i], f_pop.name))
-    j_mutation.uses(f_mut_out, link=Link.OUTPUT, transfer=True)
+    j_mutation.uses(f_mut_out, link=Link.OUTPUT, transfer=True, register=False)
     
     workflow.addJob(j_mutation)
     workflow.depends(j_mutation, individuals_merge_jobs[i])
@@ -171,7 +165,7 @@ for i in range(len(individuals_files)):
     j_freq.uses(f_columns, link=Link.INPUT)
 
     f_freq_out = File('chr%s-%s-freq.tar.gz' % (c_nums[i], f_pop.name))
-    j_freq.uses(f_freq_out, link=Link.OUTPUT, transfer=True)
+    j_freq.uses(f_freq_out, link=Link.OUTPUT, transfer=True, register=False)
 
     workflow.addJob(j_freq)
     workflow.depends(j_freq, individuals_merge_jobs[i])
